0008f659d7959ee2e57c1d591e6dc7e6beb0b910
mgetz34 <mgetz340@gmail.com>
2022-11-17 13:46:49 -0700

    added file

--- 09-NodeJS/01-Activities/20-Stu_Inquirer-Users/index.js
+++ 09-NodeJS/01-Activities/20-Stu_Inquirer-Users/index.js
@@ -1,4 +1,4 @@
-const
+const inquirer = require("inquirer")
 const fs = require("fs")
 const { default: inquirer } = require("inquirer")
 
@@ -10,8 +10,21 @@ inquirer
             name: "username",
         },
         {
-            type: "list",
+            type: "checkbox",
             message: "what languages do you know?",
+            choices: ["js", "java", "python", "c#"],
             name: "username",
         },
+        {
+            type: "list",
+            message: "what do you like for communication",
+            choices: ["email", "text", "pony-express", "phone call"],
+            name: "communication",
+        },
     ])
+    .then((response) => {
+        // console.log(response)
+        fs.writeFile("user.txt", JSON.stringify(response), (err) => {
+            err ? console.error(err) : console.log("success");
+        });
+    });
---
+++ 09-NodeJS/01-Activities/28-Stu_Mini-Project/index.js
@@ -0,0 +1,15 @@
+//bring in inquirer and fs
+
+//prompt the user for questions 
+
+//verify the supplied answers are in an object from inquirer
+
+//take the responses and dynamically create an index.html file w/ the fs mdoule 
+
+//take the responses and call a function that returns a template string with the dynamic data
+
+//in a template string is what i am going to provide as data to the second argument of fs.write file
+
+//pull all of the html in a temoplate string
+
+const fileData = `<h1>${}</h1?`
