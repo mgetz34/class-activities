a73fd8f1da55bf9bd24b0dc0a0780b2c457fbd21
mgetz34 <mgetz340@gmail.com>
2022-11-17 14:18:33 -0700

    added files

--- 09-NodeJS/01-Activities/28-Stu_Mini-Project/index.js
+++ 09-NodeJS/01-Activities/28-Stu_Mini-Project/index.js
@@ -1,6 +1,42 @@
 //bring in inquirer and fs
+const inquirer = require('inquirer');
 
 //prompt the user for questions 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'username',
+        },
+        {
+            type: 'location',
+            message: 'What is your location?',
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: 'What is you Bio?',
+            name: 'confirm',
+        }, 
+            type: 'input',
+            message: 'What is your LI?',
+            name: 'url',
+        }, 
+            type: 'input',
+            message: 'What is your GitHub URL?',
+            name: 'url',
+        },
+        
+        
+    ])
+    .then((response) =>
+        response.confirm === response.password
+            ? console.log('Success!')
+            : console.log('You forgot your password already?!')
+    );
+
+
 
 //verify the supplied answers are in an object from inquirer
 
@@ -12,4 +48,4 @@
 
 //pull all of the html in a temoplate string
 
-const fileData = `<h1>${}</h1?`
+// const fileData = `<h1>${}</h1?`
