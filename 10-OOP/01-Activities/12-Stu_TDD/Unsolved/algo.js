4f347a824da795a9613d02462f46e56bda0cc9f8
mgetz34 <mgetz340@gmail.com>
2022-11-21 11:31:26 -0700

    added files

--- 10-OOP/01-Activities/12-Stu_TDD/Unsolved/algo.js
+++ 10-OOP/01-Activities/12-Stu_TDD/Unsolved/algo.js
@@ -1,9 +1,30 @@
-function Algo() {}
+function Algo() { }
 
-Algo.prototype.reverse = function(str) {};
+Algo.prototype.reverse = function (str) {
+    const newStr = str.split('').reverse().join('')
 
-Algo.prototype.isPalindrome = function(str) {};
+    return newStr
+};
 
-Algo.prototype.capitalize = function(str) {};
+Algo.prototype.isPalindrome = function (str) {
+    const revStr = str.split('').reverse().join('')
 
+    if (revStr === str) {
+        return true
+    } else {
+        return false
+    }
+};
+
+Algo.prototype.capitalize = function (str) {
+    const wordArr = str.split(' ');
+    const cleanArr = [];
+    for (const word of wordArr) {
+        const singleWord = word.split();
+        const firstLetter = singleWord.splice(0, 1)[0].toUpperCase();
+        singleWord.unShift(firstLetter);
+        cleanArr.push(singleWord.join(''));
+        return cleanArr.join(' ');
+    }
+};
 module.exports = Algo;
