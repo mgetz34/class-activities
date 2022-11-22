675a6aa8cd89af59a287677c2836a037c37f3d3a
mgetz34 <mgetz340@gmail.com>
2022-11-22 14:12:16 -0700

    added file

--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Word.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Word.js
@@ -1,7 +1,20 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+    constructor(currentWord) {
+        this.letters = currentWord.split(' ').map(letter =>
+            new Letter(letter));
+    }
+
+    guessLetter(letterStr) {
+        let final = false
+        this.letters.array.forEach(letterObj) => {
+            if (letterObj.guess(letterStr)) {
+                final = true;
+            };
+        });
+        final = false;
+    }
 }
 
 module.exports = Word;
