905d73bac74e93173a2be8d7fbb8f83a84e79a7d
mgetz34 <mgetz340@gmail.com>
2022-11-30 12:44:07 -0700

    edited files 12.1 - 12.5 following along in class

--- 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
+++ 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,8 @@
 -- Write code here or enter directly in MySQL shell --
+DROP DATABASE IF EXISTS sample_db;
+DROP DATABASE IF EXISTS books_db;
+
+CREATE DATABASE books_db;
+
+
+USE books_db;
--- 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -4,3 +4,21 @@ CREATE DATABASE books_db;
 USE books_db;
 
 -- Add biography table --
+DROP DATABASE IF EXISTS books_db;
+-- Creates the "books_db" database --
+CREATE DATABASE books_db;
+USE books_db;
+
+-- Add biography table --
+CREATE TABLE biography(
+id INT NOT NULL,
+name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO biography(id, name) VALUES
+(1, "Monkey Wrench Gang"),
+(2, "To kill a Mockingbird"),
+(3, "Holes"),
+(4, "Stargirl"),
+(5, "Crime and Punishment"),
+(6, "The Stranger");
